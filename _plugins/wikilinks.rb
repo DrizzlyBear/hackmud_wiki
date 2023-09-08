d065899ac9e2c49d067d11da1908a39122cdd22a
20k <icedshot@gmail.com>
2023-09-08 20:39:38 +0100

    remove _notes support

--- _plugins/wikilinks.rb
+++ _plugins/wikilinks.rb
@@ -4,10 +4,9 @@ class BidirectionalLinksGenerator < Jekyll::Generator
     graph_nodes = []
     graph_edges = []
 
-    all_notes = site.collections['notes'].docs
     all_pages = site.pages
 
-    all_docs = all_notes + all_pages
+    all_docs = all_pages
 
     link_extension = !!site.config["use_html_extension"] ? '.html' : ''
 
@@ -71,32 +70,6 @@ class BidirectionalLinksGenerator < Jekyll::Generator
             <span class='invalid-link-brackets'>]]</span></span>
         HTML
       )
-    end
-
-    # Identify note backlinks and add them to each note
-    all_notes.each do |current_note|
-      # Nodes: Jekyll
-      notes_linking_to_current_note = all_notes.filter do |e|
-        e.content.include?(current_note.url)
-      end
-
-      # Nodes: Graph
-      graph_nodes << {
-        id: note_id_from_note(current_note),
-        path: "#{site.baseurl}#{current_note.url}#{link_extension}",
-        label: current_note.data['title'],
-      } unless current_note.path.include?('_notes/index.html')
-
-      # Edges: Jekyll
-      current_note.data['backlinks'] = notes_linking_to_current_note
-
-      # Edges: Graph
-      notes_linking_to_current_note.each do |n|
-        graph_edges << {
-          source: note_id_from_note(n),
-          target: note_id_from_note(current_note),
-        }
-      end
     end
 
     File.write('_includes/notes_graph.json', JSON.dump({
