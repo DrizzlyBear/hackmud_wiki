084ccef261c45566a0c82013ea7a9795396ec539
20k <icedshot@gmail.com>
2023-09-09 21:15:55 +0100

    stop wikilinks from continuously regenerating the server

--- _plugins/wikilinks.rb
+++ _plugins/wikilinks.rb
@@ -103,11 +103,6 @@ class BidirectionalLinksGenerator < Jekyll::Generator
         HTML
       )
     end
-
-    File.write('_includes/notes_graph.json', JSON.dump({
-      edges: graph_edges,
-      nodes: graph_nodes,
-    }))
   end
 
   def note_id_from_note(note)
