bff00ecf1a4882d34e128037c32818da52857df5
20k <icedshot@gmail.com>
2023-09-09 22:49:53 +0100

    regex colour replacement for user.scriptname, key:value

--- _plugins/colouring.rb
+++ _plugins/colouring.rb
@@ -14,9 +14,11 @@ class WordColour < Jekyll::Generator
             #"hello"
             #current_page.content = current_page.content.gsub(/("[A-Za-z0-9]*")/i, '<span class="text_value">\1</span>')
             
-            #
-            
+            #asdf_1234 : value
             current_page.content = current_page.content.gsub(/([A-Za-z0-9_]*)[ ]*:[ ]*("+[A-Za-z0-9_]*"+)/i, '<span class="text_key">\1</span>:<span class="text_value">\2</span>')
+            
+            #asdf_1234.hello_there
+            current_page.content = current_page.content.gsub(/([A-Za-z0-9_]*)\.([A-Za-z0-9_]*)/i, '<span class="text_username">\1</span>.<span class="text_script">\2</span>')
       end
    end
 end
