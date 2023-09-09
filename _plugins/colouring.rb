8e5b4c9d00e0b03fb96146acba110c7d8c7aecea
20k <icedshot@gmail.com>
2023-09-09 23:29:29 +0100

    fix colouring so it doesn't autocolour any word ending in a .

--- _plugins/colouring.rb
+++ _plugins/colouring.rb
@@ -17,15 +17,15 @@ class WordColour < Jekyll::Generator
             trust_users = ["accts", "autos", "binmat", "chats", "corps", "escrow", "gui", "kernel", "market", "scripts", "sys", "trust", "users"]
             
             #asdf_1234 : value
-            current_page.content = current_page.content.gsub(/([A-Za-z0-9_]*)[ ]*:[ ]*("+[A-Za-z0-9_]*"+)/i, '<span class="text_key">\1</span>:<span class="text_value">\2</span>')
+            current_page.content = current_page.content.gsub(/([A-Za-z0-9_]+)[ ]*:[ ]*("+[A-Za-z0-9_]*"+)/i, '<span class="text_key">\1</span>:<span class="text_value">\2</span>')
             
             trust_users.each do |name |
                 #trust.hello_there
-                current_page.content = current_page.content.gsub(/(#{name})\.([A-Za-z0-9_]*)/, '<span class="text_trustuser">\1</span>.<span class="text_script">\2</span>')
+                current_page.content = current_page.content.gsub(/(#{name})\.([A-Za-z0-9_]+)/, '<span class="text_trustuser">\1</span>.<span class="text_script">\2</span>')
             end
             
             #asdf_1234.hello_there
-            current_page.content = current_page.content.gsub(/([A-Za-z0-9_]*)\.([A-Za-z0-9_]*)/i, '<span class="text_username">\1</span>.<span class="text_script">\2</span>')
+            current_page.content = current_page.content.gsub(/([A-Za-z0-9_]+)\.([A-Za-z0-9_]+)/i, '<span class="text_username">\1</span>.<span class="text_script">\2</span>')
       end
    end
 end
